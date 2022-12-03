54e6e0b04a37df6b892170764e7b4b54e4c59a59
VasenkovAA <114131027+VasenkovAA@users.noreply.github.com>
2022-12-03 21:47:50 +0300

    Add files via upload
    final project

--- HomeWork2/HW2/HW2.cpp
+++ HomeWork2/HW2/HW2.cpp
@@ -39,12 +39,37 @@ float calculateDye(struct Dimension sizes, float consumption) {
 	float dye_needed = square * consumption;
 	return dye_needed;
 }
+//расчет стоимости покраски
+float calculatePaintingCost(struct Dimension sizes, float consumption, float
+	workCost, float paintCost) {
+	return (calculateDye(sizes, consumption) * paintCost * workCost*0.001);
+}
 //расчет стоимости шкафа
-void calculateCupboardCost(struct Dimension sizes, float cupboardCost) {
+float calculateCupboardCost(struct Dimension sizes, float cupboardCost) {
+	return (calculateSquare(sizes)* cupboardCost);
+}
+//расчет массы шкафа
+float calculateMass(struct Dimension sizes, float weight) {
+	return (calculateSquare(sizes) * weight);
+}
+//расчет подьема
+float calculateMss(int floorsCount, float riseCost) {
+	return (floorsCount * riseCost);
+}
 
+//расчет полный
+float calculateCost(struct Dimension sizes, float cupboardCost, float paintConsumptio, float paintCost,
+	float paintWorkCost, float weight, float floorsCount, int riseCost,float deliveryCost) {
+	float full_cost = (calculatePaintingCost(sizes, paintConsumptio, paintWorkCost, paintCost));
+	full_cost = full_cost + (calculateCupboardCost(sizes, cupboardCost));
+	full_cost = full_cost + (deliveryCost);
+	return full_cost;
 }
 
 int main() {
+
+	struct Dimension cupboard = {10,15,20};
+
 	// расход краски, стоимость работы по покраске, стоимость краски
 	float paintConsumption = 0.8, paintWorkCost = 35.00, paintCost = 50.00;
 	// стоимость материала шкафа за м2
@@ -53,11 +78,16 @@ int main() {
 	float weight = 0.9;
 	// число этажей
 	int floorsCount = 5;
-	// float deliveryCost = 500.00;
-	// float riseCost = 2.50;
+	float deliveryCost = 500.00;
+	float riseCost = 2.50;
 
-
-
-
+	std::cout << "Cost by position:" << std::endl;
+	std::cout << " - cupboard cost:" << calculateCupboardCost (cupboard, cupboardCost) << std::endl;
+	std::cout << " - painting cost:" << calculatePaintingCost(cupboard, paintConsumption, 
+		paintWorkCost, paintCost) << std::endl;
+	std::cout << " - delivery cost (with rise):" << calculateMss(floorsCount, riseCost) << std::endl;
+	std::cout << "Total cost :" << calculateCost(cupboard, cupboardCost, paintConsumption, paintCost,
+		paintWorkCost, weight, floorsCount, riseCost, deliveryCost) << std::endl;
+	
 
 }
